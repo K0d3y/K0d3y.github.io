7b3b1898d1521bd62ff863206fef260977136b81
K0d3y <125231413+K0d3y@users.noreply.github.com>
2023-08-07 00:55:15 +0800

    Final Update

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ page3btn.addEventListener("click", function () {
   show(3);
 });
 
+
 /*for hamMenu */
 const hamBtn=document.querySelector("#hamIcon");
 hamBtn.addEventListener("click",toggleMenus);
@@ -37,22 +38,30 @@ hamBtn.addEventListener("click",toggleMenus);
 const menuItemsList=document.querySelector("nav div");
 function toggleMenus(){ /*open and close menu*/
   if(menuItemsList.style.display=="block")
+  {
+    hamBtn.style.background = "brown";
     menuItemsList.style.display="none";
+  }
   else 
+  {
+    hamBtn.style.background = "darkred";
     menuItemsList.style.display="block";
+  }
 }//can optimize using toggle class with css transitions
+
+
 
 const cube = document.querySelector(".cube");
 const scene = document.querySelector(".scene");
 scene.addEventListener("click",showNextFace);
 //target all elements to save to constants
 var allText=document.querySelectorAll(".ForgeText");
-//select all subtopic pages
+//select all subtext
 hideallText();
 cube.style.transform = "translateZ(-200px) rotateY(  0deg)";
 showText(1);
-function hideallText(){ //function to hide all pages
-  for(let oneText of allText){ //go through all subtopic pages
+function hideallText(){ //function to hide all
+  for(let oneText of allText){ //go through all subtext
     oneText.style.display="none"; //hide it
   }
 }
@@ -60,10 +69,11 @@ function showText(txtno){ //function to show selected page no
   hideallText();
   //select the page based on the parameter passed in
   let oneText=document.querySelector("#Forge"+txtno); 
-    //show the page
+    //show
     oneText.style.display="inline-block";
 }
 let count = 1;
+// sunction to turn cube and show appropriate text
 function showNextFace(){
   // show front
   if (count == 0)
@@ -106,5 +116,73 @@ function showNextFace(){
     cube.style.transform = "translateZ(-200px) rotateX(  90deg)";
     count = 0;
     showText(6);
+  }
+}
+
+const cube2 = document.querySelector(".cube2");
+const scene2 = document.querySelector(".scene2");
+scene2.addEventListener("click",showNextFace2);
+//target all elements to save to constants
+var allText2=document.querySelectorAll(".HistoryText");
+//select all subtext
+hideallText2();
+cube2.style.transform = "translateZ(-200px) rotateY(  0deg)";
+showText2(1);
+function hideallText2(){ //function to hide all
+  for(let oneText of allText2){ //go through all subtext
+    oneText.style.display="none"; //hide it
+  }
+}
+function showText2(txtno){ //function to show selected page no
+  hideallText2();
+  //select the page based on the parameter passed in
+  let oneText=document.querySelector("#History"+txtno); 
+    //show
+    oneText.style.display="inline-block";
+}
+let count2 = 1;
+// sunction to turn cube and show appropriate text
+function showNextFace2(){
+  // show front
+  if (count2 == 0)
+  {
+    cube2.style.transform = "translateZ(-200px) rotateY(  0deg)";
+    count2++;
+    showText2(1);
+  }
+  // show right
+  else if (count2 == 1)
+  {
+    cube2.style.transform = "translateZ(-200px) rotateY(  -90deg)";
+    count2++;
+    showText2(2);
+  }
+  // show back
+  else if (count2 == 2)
+  {
+    cube2.style.transform = "translateZ(-200px) rotateY(  -180deg)";
+    count2++;
+    showText2(3);
+  }
+  // show left
+  else if (count2 == 3)
+  {
+    cube2.style.transform = "translateZ(-200px) rotateY(  90deg)";
+    count2++;
+    showText2(4);
+  }
+  // show top
+  else if (count2 == 4)
+  {
+    cube2.style.transform = "translateZ(-200px) rotateX(  -90deg)";
+    count2++;
+    showText2(5);
+  }
+  // show bottom
+  else if (count2 == 5)
+  {
+    cube2.style.transform = "translateZ(-200px) rotateX(  90deg)";
+    count2 = 0;
+    showText2(6);
   }
 }
